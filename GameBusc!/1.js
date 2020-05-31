92954697b6cf95cb52f72463e6eb62f42a19a5b5
ANDERAAM <48502025+ANDERAAM@users.noreply.github.com>
2020-05-30 23:35:01 -0500

    Version 3.0

--- GameBusc!/1.js
+++ GameBusc!/1.js
@@ -1,5 +1,5 @@
 
-var Nivel;var Nombre;
+var Nivel;var Nombre;var Cont=0;
 
 function Inicio(){
   Nivel  = document.getElementById("NivelJuego").value;
@@ -11,29 +11,45 @@ function Inicio(){
 }
 
 function TableroF(){
-  alert("Entro a facil");
-document.getElementById("CTOTAL").style.display="none";
-var c=0;
-var body = document.getElementsByTagName("body")[0];
-var tabla = document.createElement("table");
-var tblBody = document.createElement("tbody");
-for (var i = 0; i < 4; i++){
-var fila = document.createElement("tr");
-for (var j = 0; j < 4;j++){
-var celda = document.createElement("td");
-var Blo   = document.createElement("img");
-Blo.src="Bloque.png";
-celda.appendChild(Blo);c++;
-//alert("Hola_Mundo");
-celda.setAttribute("id","Blo");
-//alert("Hola_Mundo");
-fila.appendChild(celda);
+ alert("Entro a facil");
+ document.getElementById("CTOTAL").style.display="none";
+ var c=0;
+
+ var body = document.getElementsByTagName("body")[0];
+ var tabla = document.createElement("table");
+ var tblBody = document.createElement("tbody");
+
+ for (var i = 0; i < 4; i++){
+ var fila  = document.createElement("tr");
+ for (var j = 0; j < 4; j++){
+ var celda =  document.createElement("td");
+ var Img   =  document.createElement("img");
+ Img.src="Bloque.png"; c=c+1;
+ Img.setAttribute("id","ID"+c);
+ celda.appendChild(Img);
+ fila.appendChild(celda);
+ }
+ tblBody.appendChild(fila);
+ }
+ tabla.appendChild(tblBody);
+ TableroCompleto.appendChild(tabla);
+ tabla.setAttribute('Class','Tabla');
+ tabla.setAttribute('border','10');
+
+  Cont = Cont + 1 ;
+  document.getElementById("ID0").onclick = function() {
+  document.getElementById("ID0").innerHTML = alert("YOU CLICKED ME! : " + "ID0");
+
 }
-tblBody.appendChild(fila);
+
+/*function ClickImg(){
+  Cont = Cont + 1 ;
+  document.getElementById("ID"+Cont).onclick = function() {
+    document.getElementById("ID"+Cont).innerHTML = alert("YOU CLICKED ME! : "+"ID"+Cont);
+  };
 }
-/*tabla.appendChild(tblBody);
-TableroCompleto.appendChild(tabla);
-tabla.setAttribute('class','Tabla');
-tabla.setAttribute('border','1');
 */
-}
+
+
+
+//ClickImg();
